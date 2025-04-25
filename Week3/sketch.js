b8822105000fb23196af94bca7fc1ba51a3bfcc1
coryMcKague <cory.mckague@umt.edu>
2025-04-25 11:29:28 -0600

    Update sketch.js

--- Week3/sketch.js
+++ Week3/sketch.js
@@ -1,14 +1,17 @@
-var leftGuitar;
-
-
-function preload(){
-
-    leftGuitar = loadSound('./musicSources/bass.wav');
-
-
+let mySound;
+function preload() {
+  mySound = loadSound('./musicSources/bass.wav');
 }
 
-function setup(){
-    createCanvas(200,200);
-    leftGuitar.play();
+function setup() {
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(canvasPressed);
+  background(220);
+  text('tap here to play', 10, 20);
+}
+
+function canvasPressed() {
+  // playing a sound file on a user gesture
+  // is equivalent to `userStartAudio()`
+  mySound.play();
 }
